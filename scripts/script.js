81b86c0a67ca6ae0e7b3eb256477e90075d4ba6e
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-06-02 15:58:48 -0400

    started to create event-listener logic in JavaScript

--- scripts/script.js
+++ scripts/script.js
@@ -1,67 +1,131 @@
-
-// DOCUMENT READY
-$(() => {
-
+// APP NAMESPACE OBJECT
 const app = {};
 
 // VARIABLES
 
-// app.$form = $('.questionForm');
-app.$prevBtn = $('.previousBtn');
-app.$nextBtn = $('.nextBtn');
+let choice = "";
+let currentForm = "";
 
-app.$questionFormOne = $('.questionFormOne');
-app.$questionFormTwoA = $('.questionFormTwoA'); 
-app.$questionFormThreeA = $('.questionFormThreeA');
-
-app.userAnswers = [];
+app.trees = {
+  balsalmFir: {
+    name: "Balsalm Fir",
+    desc: "balsalm paragraph"
+  },
+  hemlock: {
+    name: "Hemlock",
+    desc: "hemlock paragraph"
+  }, 
+  whitePine: {
+    name: "White Pine",
+    desc: "white pine"
+  },
+  redPine: {
+    name: "Red Pine",
+    desc: "red pine"
+  }, 
+  jackPine: {
+    name: "Jack Pine",
+    desc: "jack pine"
+  },
+  scotchPine: {
+    name: "Scotch Pine",
+    desc: "scotch pine"
+  },
+  larch: {
+    name: "Larch",
+    desc: "larch"
+  },
+  easternWhiteCedar: {
+    name: "Eastern White Cedar",
+    desc: "eastern white cedar"
+  },
+  easternRedCedar: {
+    name: "Eastern Red Cedar",
+    desc: "eastern red cedar"
+  }
+}
 
 // FUNCTIONS 
 
-app.handleNext = function() {
-
-  const $form = $('.questionForm');
-  
-  const selectedItem = $form.find("input[type='radio']:checked");
-
-  console.log(selectedItem);
-
-  // const choice = selectedItem.classList[0];
-
-  // console.log(choice);
-
-  // if (choice === "needles") {
-  //   app.$questionFormOne.toggleClass('questionFormActive');
-  //   app.$questionFormTwoA.toggleClass('questionFormActive');
-  // } else if (choice === "rounded") {
-
-  // } else if (choice === "single") {
-  //   app.$questionFormTwoA.toggleClass('questionFormActive');
-  //   app.$questionFormThreeA.toggleClass('questionFormActive');
-  // }
+// Next Button Function
+app.handleNext = function () {
 
 
-  
-  
-  // app.$form.html(`
-  //   <label for="optionOne">
-  //     <input type="radio" id="optionOne" class="optionOne" name="questionOne">
-  //     <span>Are the leaves shaped like needles?</span>
-  //   </label>
-  // `);
+
+  if (choice === "needles") {
+    app.$questionFormOne.toggleClass('questionFormActive');
+    app.$questionFormTwoA.toggleClass('questionFormActive');
+  } else if (choice === "single") {
+    app.$questionFormTwoA.toggleClass('questionFormActive');
+    app.$questionFormThreeA.toggleClass('questionFormActive');
+  } else if (choice === "flat") {
+    app.$questionFormThreeA.toggleClass('questionFormActive');
+    app.$questionNeedlesSingleFlat.toggleClass('questionFormActive');
+  } else if (choice === "angled") {
+    app.$questionFormThreeA.toggleClass('questionFormActive');
+    app.$questionNeedlesSingleAngled.toggleClass('questionFormActive');
+  } else if (choice === "grouped") {
+    app.$questionFormTwoA.toggleClass('questionFormActive');
+    app.$questionNeedlesGrouped.toggleClass('questionFormActive');
+  } else if (choice === "rounded") {
+    console.log("yes");
+    app.$questionFormOne.toggleClass('questionFormActive');
+    app.$questionRounded.toggleClass('questionFormActive');
+  } else if (currentForm.classList.contains('questionFormTree')) {
+    currentForm.remove('questionFormActive');
+    app.$treeSlide.toggleClass('treeSlideActive');
+
+    const treeName = app.trees[choice].name;
+    const treeDesc = app.trees[choice].desc;
+
+    app.$treeSlide.find('h2')[0].innerHTML = treeName;
+    app.$treeSlide.find('p')[0].innerHTML = treeDesc;
+  }
+
 }
 
+// Radio Button Select Function
+app.selectQuestion = function(e) {
+  // When a choice has been selected, this will put an event listener on the next button
+  // NOTE: I should change the styling so it officially looks interactive
+  app.$nextBtn.off('click', app.handleNext);
+  app.$nextBtn.on('click', app.handleNext);
 
+  choice = e.target.className;
+  console.log(choice);
+  currentForm = e.target.form;
+  console.log(currentForm.classList.contains('questionFormTree'));
 
+}
 
+app.init = function () {
 
+  // SELECTOR VARIABLES
 
-// EVENT LISTENERS
+  app.$form = $('.questionForm');
+  app.$prevBtn = $('.previousBtn');
+  app.$nextBtn = $('.nextBtn');
+  app.$radio = $('input[type="radio"]');
 
-app.$nextBtn.on('click', app.handleNext);
+  app.$questionFormOne = $('.questionFormOne');
+  app.$questionFormTwoA = $('.questionFormTwoA');
+  app.$questionFormThreeA = $('.questionFormThreeA');
 
+  app.$questionNeedlesSingleFlat = $('.questionNeedlesSingleFlat');
+  app.$questionNeedlesSingleAngled = $('.questionNeedlesSingleAngled');
+  app.$questionNeedlesGrouped = $('.questionNeedlesGrouped');
+  app.$questionRounded = $('.questionRounded');
 
-});
+  app.$treeSlide = $('.treeSlide');
+
+  // EVENT LISTENERS
+
+  app.$form.on('click', 'input[type="radio"]', app.selectQuestion);
+
+};
+
+// DOCUMENT READY
+$(() => { app.init() });
 
 
 
