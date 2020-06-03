ec598d4167608c22e875a52b53491c188c8d97e1
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-06-03 08:42:55 -0400

    worked on previous button functionality

--- scripts/script.js
+++ scripts/script.js
@@ -49,8 +49,33 @@ app.trees = {
 
 // Previous Button Function 
 app.prevBtn = function() {
+  // app.$nextBtn.on('click', app.handleNext);
+
+  // Finds the current form displaying, and gets the first input
   const currentForm = app.$questions.find('.questionFormActive')[0];
+  const inputName = currentForm[0].name;
+
+  const currentSlide = app.$questions.find('.treeSlideActive')[0];
+
   console.log(currentForm);
+  console.log(currentSlide);
+
+  if (inputName === "levelOne") {
+    console.log("Nope");
+  } else if (inputName === "levelTwoA") {
+    currentForm.classList.toggle('questionFormActive');
+    app.$questionFormOne.toggleClass('questionFormActive');
+    app.$questionFormOne[0].reset();
+    console.log(app.$questionFormOne);
+  } else if (inputName === "levelThreeA") {
+    currentForm.classList.toggle('questionFormActive');
+    app.$questionFormTwoA.toggleClass('questionFormActive');
+    app.$questionFormTwoA[0].reset();
+  } else if (inputName === "needlesSingleFlat" || inputName === "needlesSingleAngled" || inputName === "needlesGrouped") {
+    currentForm.classList.toggle('questionFormActive');
+    app.$questionFormTwoA.toggleClass('questionFormActive');
+    app.$questionFormTwoA[0].reset();
+  } 
 }
 
 // Next Button Function
@@ -74,6 +99,9 @@ app.handleNext = function () {
   } else if (choice === "grouped") {
     app.$questionFormTwoA.toggleClass('questionFormActive');
     app.$questionNeedlesGrouped.toggleClass('questionFormActive');
+  } else if (choice === "clustered") {
+    app.$questionFormTwoA.toggleClass('questionFormActive');
+    app.$questionNeedlesClustered.toggleClass('questionFormActive');
   } else if (choice === "rounded") {
     app.$questionFormOne.toggleClass('questionFormActive');
     app.$questionRounded.toggleClass('questionFormActive');
@@ -97,9 +125,7 @@ app.selectQuestion = function(e) {
   app.$nextBtn.on('click', app.handleNext);
 
   choice = e.target.className;
-  console.log(choice);
   currentForm = e.target.form;
-  console.log(currentForm.classList.contains('questionFormTree'));
 
 }
 
@@ -120,6 +146,7 @@ app.init = function () {
   app.$questionNeedlesSingleFlat = $('.questionNeedlesSingleFlat');
   app.$questionNeedlesSingleAngled = $('.questionNeedlesSingleAngled');
   app.$questionNeedlesGrouped = $('.questionNeedlesGrouped');
+  app.$questionNeedlesClustered = $('.questionNeedlesClustered');
   app.$questionRounded = $('.questionRounded');
 
   app.$treeSlide = $('.treeSlide');
